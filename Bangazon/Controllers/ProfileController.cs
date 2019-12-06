d6a1d8552765fadd0f6f3ca30e93fd672e676a2c
unknown <mstiles01@icloud.com>
2019-12-06 07:31:14 -0600

    Wrote logic for edit

--- Bangazon/Controllers/ProfileController.cs
+++ Bangazon/Controllers/ProfileController.cs
@@ -6,6 +6,7 @@ using Bangazon.Data;
 using Bangazon.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bangazon.Controllers
 {
@@ -24,13 +25,88 @@ namespace Bangazon.Controllers
         // GET: Products/Details/5
         public async Task<IActionResult> ProfileDetails()
         {
-      
+
             var user = await _userManager.GetUserAsync(HttpContext.User);
-            
-            user.PaymentTypes =  _context.PaymentType
+
+            user.PaymentTypes = _context.PaymentType
                .Where(p => p.UserId == user.Id).ToList();
-            
+
             return View(user);
-        } 
+        }
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            user.Id = id;
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.C:\Users\marla\workspace\cSharp\groupProjects\bangazon-site-cotton-headed-ninny-muggins\Bangazon\Views\Users\
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, ApplicationUser applicationUser)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            user.FirstName = applicationUser.FirstName;
+            user.LastName = applicationUser.LastName;
+            user.StreetAddress = applicationUser.StreetAddress;
+            user.PhoneNumber = applicationUser.PhoneNumber;
+
+
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    IdentityResult result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                        return RedirectToAction(nameof(ProfileController));
+                    else
+                    {
+                        foreach (IdentityError error in result.Errors)
+                            ModelState.AddModelError("", error.Description);
+                    }
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(ProfileController));
+            }
+            return View(user);
+        }
+
+
+
+        private bool UserExists(string id)
+        {
+            return _context.ApplicationUsers.Any(e => e.Id == id);
+        }
     }
-} 
+}
+    
